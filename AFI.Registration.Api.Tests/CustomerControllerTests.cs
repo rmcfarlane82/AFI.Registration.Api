bf28641283f906f4e90f79a2a0d1045c9b2e9a11
Rory McFarlane <Rory.McFarlane@hgem.com>
2020-12-20 15:47:26 +0000

    adding placeholder tests

---
+++ AFI.Registration.Api.Tests/CustomerControllerTests.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace AFI.Registration.Api.Tests
+{
+    public class CustomerControllerTests
+    {
+        [Fact]
+        public void When_sending_a_invalid_registration_request_THEN_bad_request_is_returned()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void When_sending_a_valid_registration_request_THEN_customer_id_is_returned()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void When_exception_is_thrown_THEN_internal_server_error_is_returned()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
