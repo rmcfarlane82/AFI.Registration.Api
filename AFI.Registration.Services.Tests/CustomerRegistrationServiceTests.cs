4f716ce9777801bb0ee87e8c23098d1d50a886b6
Rory McFarlane <Rory.McFarlane@hgem.com>
2020-12-20 14:07:05 +0000

    adding first customer service validation unit test

--- AFI.Registration.Services.Tests/CustomerRegistrationServiceTests.cs
+++ AFI.Registration.Services.Tests/CustomerRegistrationServiceTests.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AFI.Registration.Services.Models;
 using AFI.Test.Helpers;
+using AutoFixture.Xunit2;
 using FluentAssertions;
 using FluentValidation;
+using FluentValidation.Results;
+using NSubstitute;
 using Xunit;
 
 namespace AFI.Registration.Services.Tests
@@ -13,9 +17,13 @@ namespace AFI.Registration.Services.Tests
         [Theory, AutoNSubstituteData]
         public void When_registration_request_is_not_valid_THEN_validation_exception_is_thrown(
             RegistrationRequest registrationRequest,
+            List<ValidationFailure> validationFailures,
+            [Frozen]IValidator<RegistrationRequest> registrationRequestValidator,
             CustomerRegistrationService sut
             )
         {
+            registrationRequestValidator.ValidateAsync(registrationRequest).Returns(new ValidationResult(validationFailures));
+
             Func<Task> act = () => sut.Register(registrationRequest);
 
             act.Should().Throw<ValidationException>();
--- AFI.Registration.Services/CustomerRegistrationService.cs
+++ AFI.Registration.Services/CustomerRegistrationService.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Threading.Tasks;
 using AFI.Registration.Services.Models;
+using FluentValidation;
 
 namespace AFI.Registration.Services
 {
     public class CustomerRegistrationService : ICustomerRegistrationService
     {
-        public Task Register(RegistrationRequest registrationRequest)
+        private readonly IValidator<RegistrationRequest> _registrationRequestValidator;
+
+        public CustomerRegistrationService(IValidator<RegistrationRequest> registrationRequestValidator)
         {
+            _registrationRequestValidator = registrationRequestValidator;
+        }
+
+        public async Task Register(RegistrationRequest registrationRequest)
+        {
+            var validationResult = await _registrationRequestValidator.ValidateAsync(registrationRequest);
+
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult.Errors);
+            }
+
             throw new NotImplementedException();
         }
     }
