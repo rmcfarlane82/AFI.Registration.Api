b077ff659abd9c160dbdf628e94622c493cd6dab
Rory McFarlane <Rory.McFarlane@hgem.com>
2020-12-20 12:55:37 +0000

    adding attributes for auto mocking

---
+++ AFI.Test.Helpers/AutoNSubstituteDataAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using AutoFixture.Xunit2;
+
+namespace AFI.Test.Helpers
+{
+    public class AutoNSubstituteDataAttribute : AutoDataAttribute
+    {
+        public AutoNSubstituteDataAttribute(params Type[] optionalCustomizationTypes)
+            : base(() => BuildFixture(optionalCustomizationTypes))
+        {
+        }
+
+        private static IFixture BuildFixture(IEnumerable<Type> optionalCustomizationTypes)
+        {
+            var fixture = new Fixture();
+
+            fixture.Customize(new AutoNSubstituteCustomization());
+
+            foreach (var customizationType in optionalCustomizationTypes)
+            {
+                fixture.Customize((ICustomization)Activator.CreateInstance(customizationType));
+            }
+
+            return fixture;
+        }
+    }
+}
---
+++ AFI.Test.Helpers/InlineAutoNSubstituteDataAttribute.cs
@@ -0,0 +1,11 @@
+﻿using AutoFixture.Xunit2;
+
+namespace AFI.Test.Helpers
+{
+    public class InlineAutoNSubstituteDataAttribute : InlineAutoDataAttribute
+    {
+        public InlineAutoNSubstituteDataAttribute(params object[] objects) : base(new AutoNSubstituteDataAttribute(), objects)
+        {
+        }
+    }
+}
