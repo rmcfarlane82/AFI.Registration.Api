890ce6118e52929ea394e8771cae717058d7dabb
Rory McFarlane <Rory.McFarlane@hgem.com>
2020-12-20 13:02:01 +0000

    adding context and type configurations for customer

---
+++ AFI.Registration.Data/Context/RegistrationContext.cs
@@ -0,0 +1,21 @@
+﻿using AFI.Registration.Data.Entities;
+using AFI.Registration.Data.TypeConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace AFI.Registration.Data.Context
+{
+    public class RegistrationContext : DbContext
+    {
+        public RegistrationContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CustomerTypeConfiguration());
+        }
+    }
+}
---
+++ AFI.Registration.Data/TypeConfiguration/CustomerTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AFI.Registration.Data.TypeConfiguration
+{
+    public class CustomerTypeConfiguration : IEntityTypeConfiguration<Entities.Customer>
+    {
+        public void Configure(EntityTypeBuilder<Entities.Customer> builder)
+        {
+            builder.HasKey(k => k.Id);
+            builder.Property(p => p.FirstName).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.LastName).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.ReferenceNumber).HasMaxLength(9).IsRequired();
+            builder.Property(p => p.EmailAddress).HasMaxLength(320);
+        }
+    }
+}
