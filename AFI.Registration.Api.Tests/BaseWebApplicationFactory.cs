6be257c6aae60460f6d10189ba29739edbf46157
Rory McFarlane <Rory.McFarlane@hgem.com>
2020-12-20 15:36:02 +0000

    adding WebApplicationFactory for api integration testing

---
+++ AFI.Registration.Api.Tests/BaseWebApplicationFactory.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace AFI.Registration.Api.Tests
+{
+    public class BaseWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup: class
+    {
+    }
+}
