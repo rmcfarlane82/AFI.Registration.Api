6e43e4c1bc4bea69ec344d2dde7f2e408724369c
Rory McFarlane <Rory.McFarlane@hgem.com>
2020-12-20 13:57:16 +0000

    adding registration request model

--- AFI.Registration.Services/Models/RegistrationRequest.cs
+++ AFI.Registration.Services/Models/RegistrationRequest.cs
@@ -1,7 +1,13 @@
-﻿namespace AFI.Registration.Services.Models
+﻿using System;
+
+namespace AFI.Registration.Services.Models
 {
     public class RegistrationRequest
     {
-
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ReferenceNumber { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string EmailAddress { get; set; }
     }
 }
