eec4655490bc25717164aba83a55b14d1f2e015b
Rory McFarlane <Rory.McFarlane@hgem.com>
2020-12-20 12:17:43 +0000

    adding base class for integration tests

---
+++ AFI.Registration.Data.Tests/IntegrationTests/BaseTestClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AFI.Registration.Data.Tests.IntegrationTests
+{
+    public class BaseTestClass<TContext> where TContext : DbContext
+    {
+        private readonly DbContextOptionsBuilder<TContext> _contextOptionsBuilder;
+
+        public BaseTestClass()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkSqlServer()
+                .BuildServiceProvider();
+
+            _contextOptionsBuilder = new DbContextOptionsBuilder<TContext>();
+
+            _contextOptionsBuilder.UseSqlServer($"Server=(localdb)\\mssqllocaldb;Database=afi_customers_db_integration_test;Trusted_Connection=True;MultipleActiveResultSets=true")
+                .UseInternalServiceProvider(serviceProvider);
+
+            using var context = GetContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+
+        public TContext GetContext()
+        {
+            return (TContext) Activator.CreateInstance(typeof(TContext), _contextOptionsBuilder.Options);
+        }
+    }
+}
