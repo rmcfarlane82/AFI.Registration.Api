073db233fa65ff8bcd265487193f1f491adf19c1
Rory McFarlane <Rory.McFarlane@hgem.com>
2020-12-20 15:15:07 +0000

    adding initial api project and test project

---
+++ AFI.Registration.Api/Controllers/CustomerRegistrationController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AFI.Registration.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerRegistrationController : ControllerBase
+    {
+    }
+}
