81df15fc809a1f68cd56f712fd18734e7a7c0d6c
Rory McFarlane <Rory.McFarlane@hgem.com>
2020-12-20 12:58:59 +0000

    adding customer entity

---
+++ AFI.Registration.Data/Entities/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+// ReSharper disable AutoPropertyCanBeMadeGetOnly.Local
+
+namespace AFI.Registration.Data.Entities
+{
+    public class Customer : EntityBase
+    {
+        // ReSharper disable once UnusedMember.Global Used by ef core
+        protected Customer(){}
+
+        public Customer(string firstName, string lastName, string referenceNumber, DateTime? dateOfBirth, string emailAddress)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            ReferenceNumber = referenceNumber;
+            DateOfBirth = dateOfBirth;
+            EmailAddress = emailAddress;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string ReferenceNumber { get; private set; }
+        public DateTime? DateOfBirth { get; private set; }
+        public string EmailAddress { get; private set; }
+    }
+}
---
+++ AFI.Registration.Data/Entities/EntityBase.cs
@@ -0,0 +1,7 @@
+﻿namespace AFI.Registration.Data.Entities
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; protected set; }
+    }
+}
